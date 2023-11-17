c808801bc4d35b5ece8b10fd219b3c0331ca8c9a
Chirag Khanna <ckhanna@horizontal.com>
2023-11-17 23:17:06 +0530

    made change for dynamic placeholder

---
+++ src/Foundation/CustomDynamicPlaceholder/Repositories/CustomWildcardReplacer.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.XA.Foundation.DynamicPlaceholders.Repositories;
+using System;
+using System.Text.RegularExpressions;
+
+namespace CustomDynamicPlaceholder.Repositories
+{
+    public class CustomWildcardReplacer : WildcardReplacer
+    {
+        private static Regex _r1;
+        static CustomWildcardReplacer()
+        {
+            _r1 = new Regex("(.)*-([0-9]+)-([\\w\\{\\}\\*]+)", RegexOptions.Compiled);
+        }
+        public override string Resolve(string key)
+        {
+            string path;
+            string last;
+            this.SplitPlaceholderKey(key, out path, out last);
+            if(_r1.Match(last).Success)
+                return path + last;
+            return base.Resolve(key);
+        }
+           
+    }
+}
